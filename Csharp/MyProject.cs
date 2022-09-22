cf34d210f3bf73fe382d44a2fe7b6a9887cd138e
cuttittejor <cuttittejor@V10RM126W10-21>
2022-09-22 08:12:53 -0700

    Add project files.

---
+++ Csharp/MyProject.cs
@@ -0,0 +1,18 @@
+﻿namespace MyProject
+{
+
+    internal class MyProject
+    {
+        static void Main(string[] args)
+        {
+            var num = 0;
+            while (num < 10)
+            {
+                num += 1;
+                Console.WriteLine(num);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Counted to ten!");
+        }
+    }
+}
